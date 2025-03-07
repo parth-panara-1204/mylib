76cf5f42c715347fc2a4e1d26c9d8fb6c4ae71c6
parth-panara-1204 <parthpanara24@gmail.com>
2025-03-07 18:33:30 +0530

    optimisers added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from ml.linearRegressor import linReg
 import numpy as np
+from time import perf_counter
 
-X = np.random.rand(10000,10)
+X = np.random.rand(5000,10)
 m = np.array([[4],
                 [8],
                 [2.6],
@@ -9,14 +10,17 @@ m = np.array([[4],
                 [-1],
                 [7.2],
                 [2.5],
-                [0.01],
+                [1.5],
                 [6.2],
                 [6.2]])
 
-y = np.matmul(X, m) - 0.001
+y = np.matmul(X, m) - 100
 
+start = perf_counter()
 l = linReg()
-l.fit(X, y)
+l.fit(X, y, lr=0.15, epochs=5, grad='NAG', batch=80)
+end = perf_counter()
 
 print(f"slope: {l.slope}")
 print(f"intersection: {l.intersect}")
+print(f"time taken: {end - start}")
--- ml/linearRegressor.py
+++ ml/linearRegressor.py
@@ -6,25 +6,109 @@ class linReg():
         self.slope = 0.0
         self.intersect = 0.0
 
-    def fit(self, X, y, lr=0.5, epochs=1000):
+    def fit(self, X, y, lr=0.5, epochs=1000, grad='miniBatchMomentum', batch=32, beta=0.9):
         self.slope = np.zeros((X.shape[1], 1))
         self.intersect = 0.0
 
-        self._gradDec(X, y, lr, epochs)
+        self._gradDec(X, y, lr, epochs, grad, batch, beta)
 
-    def _gradDec(self, X, y, lr, epochs):
+    def _gradDec(self, X, y, lr, epochs, grad, batch, beta):
         n = X.shape[0]
         
-        i = 0
-        while i < epochs:
+        # BATCH GRADIENT
+        if grad == 'batch':
+            i = 0
+
+            while i < epochs:
+                y_pred = np.matmul(X, self.slope) + self.intersect
+
+                # for least square cost
+                slopeGrad = 1/n * np.matmul( X.T, (y_pred - y))
+                intersectGrad = 1/n * np.sum(y_pred - y)
+
+                self.slope = self.slope - lr * slopeGrad
+                self.intersect = self.intersect - lr * intersectGrad
+
+                i += 1
+
             y_pred = np.matmul(X, self.slope) + self.intersect
+            print(np.mean((y_pred - y)**2))
 
-            # for least square cost
-            slopeGrad = 1/n * np.matmul( X.T, (y_pred - y))
-            intersectGrad = 1/n * np.sum(y_pred - y)
+        # MINI-BATCH GRADIENT
+        elif grad == 'miniBatch':
+            batches = n // batch
 
-            self.slope = self.slope - lr * slopeGrad
-            self.intersect = self.intersect - lr * intersectGrad
+            for i in range(epochs):
+                for j in range(batches):
+                    y_pred = np.matmul(X[j*batch:(j+1)*batch], self.slope) + self.intersect
+
+                    # for least square cost
+                    slopeGrad = 1/batch * np.matmul( X[j*batch:(j+1)*batch].T, (y_pred - y[j*batch:(j+1)*batch]))
+                    intersectGrad = 1/batch * np.sum(y_pred - y[j*batch:(j+1)*batch])
+
+                    self.slope = self.slope - lr * slopeGrad
+                    self.intersect = self.intersect - lr * intersectGrad
+
+        # BATCH GRADIENT WITH MOMENTUM
+        elif grad == 'batchMomentum':
+            i = 0
+            v1, v2 = 0, 0
+            while i < epochs:
+                y_pred = np.matmul(X, self.slope) + self.intersect
+
+                # for least square cost
+                slopeGrad = 1/n * np.matmul( X.T, (y_pred - y))
+                intersectGrad = 1/n * np.sum(y_pred - y)
+
+                v1 = (1-lr)*v1 + lr*slopeGrad
+                v2 = (1-lr)*v2 + lr*intersectGrad
+
+                self.slope = self.slope - v1
+                self.intersect = self.intersect - v2
+
+                i += 1
+
+        # MINI-BATCH GRADIENT WITH MOMENTUM
+        elif grad == 'miniBatchMomentum':
+            batches = n // batch
+
+            for i in range(epochs):
+                v1, v2 = 0, 0
+                for j in range(batches):
+                    y_pred = np.matmul(X[j*batch:(j+1)*batch], self.slope) + self.intersect
+
+                    # for least square cost
+                    slopeGrad = 1/batch * np.matmul( X[j*batch:(j+1)*batch].T, (y_pred - y[j*batch:(j+1)*batch]))
+                    intersectGrad = 1/batch * np.sum(y_pred - y[j*batch:(j+1)*batch])
+
+                    v1 = (1-lr)*v1 + lr*slopeGrad
+                    v2 = (1-lr)*v2 + lr*intersectGrad
+
+                    self.slope = self.slope - v1
+                    self.intersect = self.intersect - v2
 
             y_pred = np.matmul(X, self.slope) + self.intersect
-            i += 1
+            print(np.mean((y_pred - y)**2))
+
+
+        # NESTEROV's ACCELERATED GRADIENT
+        elif grad == 'NAG':
+            batches = n // batch
+
+            for i in range(epochs):
+                v1, v2 = 0, 0
+                for j in range(batches):
+                    la_slope = self.slope - beta*v1
+                    la_intersect = self.intersect - beta*v2
+
+                    y_pred = np.matmul(X[j*batch:(j+1)*batch], la_slope) + la_intersect
+
+                    # for least square cost
+                    slopeGrad = 1/batch * np.matmul( X[j*batch:(j+1)*batch].T, (y_pred - y[j*batch:(j+1)*batch]))
+                    intersectGrad = 1/batch * np.sum(y_pred - y[j*batch:(j+1)*batch])
+
+                    v1 = beta*v1 + lr*slopeGrad
+                    v2 = beta*v2 + lr*intersectGrad
+
+                    self.slope = self.slope - v1
+                    self.intersect = self.intersect - v2
